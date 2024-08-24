e06acb6b722bd27faee50a321b2f8e1b9b8c95a8
alexDNR <chromealexsandr@gmail.com>
2024-08-24 22:45:36 +0300

    fix code - replacement lib youtube download

--- youtube_download.py
+++ youtube_download.py
@@ -1,24 +1,12 @@
-from pytube import YouTube
-
-# функция подсчёта количества загруженных байт
-def progress_download(stream, chunk, bytes_remaining):
-    downloaded = max_file_size - bytes_remaining
-    percent = downloaded/max_file_size * 100
-    print(f"Загружено -- {percent:.2f}%")
-
-# функция окончания скачивания
-def complite (stream, path):
-    print(f'Выполнено -- {path}')
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 
 url = input('Введи URL для скачивания файла: ')
 
-yt = YouTube(url, progress_download, complite) # сюда передаём урл для скачивания и две функции остлеживания скачивания
+yt = YouTube(url, on_progress_callback = on_progress) # сюда передаём урл для скачивания и две функции остлеживания скачивания
 
 print(f"Название видео -- {yt.title}")
 print(f"Количество просмотров -- {yt.views}")
-print(f"Длительность видео -- {yt.length/60:.2f} минут")
 
 video = yt.streams.get_highest_resolution()
-max_file_size = video.filesize
-print(f'Объём файла = {max_file_size} байт')
 video.download('D:/Lessons')
